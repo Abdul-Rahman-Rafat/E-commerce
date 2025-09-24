0e8f5932dc7fc582ae3af0273c293b692beb72a4
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2025-09-24 22:25:42 +0300

    filter

--- script.js
+++ script.js
@@ -14,16 +14,46 @@ FetchDummyData().then(products => {
     products.forEach(product => {
     const card = document.createElement("div");
     card.classList.add("card");
+    card.setAttribute("data-category",product.category) // for filter
 
     card.innerHTML = `
+        <div class="icons">
+        <i class="fa-solid fa-cart-shopping fa-xl  icon"></i>
+        <i class="fa-regular fa-heart fa-xl  icon"></i>
+        </div>
         <img src="${product.image}" alt="${product.title}">
         <h3>${product.title}</h3>
         <p>$${product.price}</p>
+        
     `;
 
     container.appendChild(card);
     });
+
+
+document.querySelectorAll("#categoryFilter li").forEach(item => {
+  item.addEventListener("click", () => {
+    const selected = item.getAttribute("data-filter");
+
+    document.querySelectorAll("#categoryFilter li").forEach(li => li.classList.remove("active"));
+    item.classList.add("active");
+
+    document.querySelectorAll(".card").forEach(card => {
+      const category = card.getAttribute("data-category");
+
+      if (selected === "all" || category === selected) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+    });
+  });
 });
+  });
+
+
+
+
 
 
 
