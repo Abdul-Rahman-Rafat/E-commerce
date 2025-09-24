21f5c9b78f43b958d22683d7eee5d9c8b1155cdf
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2025-09-24 11:55:50 +0300

    add products by api

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 async function FetchDummyData() {
-try {
-    const response = await fetch('https://fakestoreapi.com/products'); 
-    const data = await response.json(); 
-    console.log(data)
-
-    return data; 
-} catch (error) {
+    try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    return data;
+    } catch (error) {
     console.error('Error while fetching data:', error);
+    }
 }
-}
-FetchDummyData()
+
+FetchDummyData().then(products => {
+    const container = document.getElementById("products");
+
+    products.forEach(product => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+        <img src="${product.image}" alt="${product.title}">
+        <h3>${product.title}</h3>
+        <p>$${product.price}</p>
+    `;
+
+    container.appendChild(card);
+    });
+});
+
+
+
+
+
+
+
+
